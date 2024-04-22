4ccb2a24782959568022bb3c3d32710af53489db
Jorge FdCD <jfdecd@icloud.com>
2024-04-22 15:02:37 +0200

    wolf_eyes_20240415_data

--- src/wolf_eyes.jl
+++ src/wolf_eyes.jl
@@ -15,3 +15,24 @@ function wolf_eyes_artr_data()
     corr_right = matread(joinpath(wolf_eyes_artr_data_dir(), "DFF_corr_Right_ARTR.mat"))["DFF_corr_Right_ARTR"]
     return (; left, right, corr_left, corr_right)
 end
+
+#= Second eye movement data, 20240415, from S.Wolf =#
+function wolf_eyes_20240415_data_dir()
+    return artifact"Wolf_Eyes_ARTR_Data_20240415"
+end
+
+function wolf_eyes_20240415_data()
+    left = matread(joinpath(wolf_eyes_20240415_data_dir(), "2014-12-19 Run 02", "ARTR neural signals", "A_Left_ARTR.mat"))["A_Left_ARTR"]
+    right = matread(joinpath(wolf_eyes_20240415_data_dir(), "2014-12-19 Run 02", "ARTR neural signals", "A_Right_ARTR.mat"))["A_Right_ARTR"]
+    corr_left = matread(joinpath(wolf_eyes_20240415_data_dir(), "2014-12-19 Run 02", "ARTR neural signals", "DFF_corr_Left_ARTR.mat"))["DFF_corr_Left_ARTR"]
+    corr_right = matread(joinpath(wolf_eyes_20240415_data_dir(), "2014-12-19 Run 02", "ARTR neural signals", "DFF_corr_Right_ARTR.mat"))["DFF_corr_Right_ARTR"]
+
+    D_struct = matread(joinpath(wolf_eyes_20240415_data_dir(), "2014-12-19 Run 02", "2014-12-19-run2.mat"))["D"]
+    position = D_struct["position"]
+    timeplusleft = D_struct["timeplusleft"]
+    timeplusright = D_struct["timeplusright"]
+    timeminusleft = D_struct["timeminusleft"]
+    timeminusright = D_struct["timeminusright"]
+
+    return (; left, right, corr_left, corr_right, position, timeplusleft, timeplusright, timeminusleft, timeminusright)
+end
