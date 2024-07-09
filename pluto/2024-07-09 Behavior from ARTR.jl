d87eeafd3d71737982548a53f0dab2b8b3b1e42b
Jorge FdCD <jfdecd@icloud.com>
2024-07-09 17:31:59 +0200

    wip

--- pluto/2024-07-09 Behavior from ARTR.jl
+++ pluto/2024-07-09 Behavior from ARTR.jl
@@ -7,6 +7,12 @@ using InteractiveUtils
 # ╔═╡ 7f95a967-426a-465f-bad4-671372ea1092
 import Pkg, Revise; Pkg.activate(Base.current_project())
 
+# ╔═╡ 7d355591-dddd-4463-97b1-d53e223bd6b3
+using Statistics: mean
+
+# ╔═╡ 8398c332-235b-4f6c-ab1e-8b5fd62d2d5a
+using Distributions: Gamma
+
 # ╔═╡ 5b3604a6-3dcc-11ef-1ff1-a71b51c7d8e9
 md"# Imports"
 
@@ -56,11 +62,54 @@ artr_viterbi_states = Dict((; temperature, fish) =>
 # ╔═╡ fd8ef759-e4e2-4a8b-8fa7-bafad992ef7a
 artr_viterbi_states[(; temperature=26, fish=6)]
 
-# ╔═╡ 369d6c55-4239-4eeb-b23b-4c3910d791fd
-md"# Load behavior data"
+# ╔═╡ 7ae067c1-8c0c-4f91-b8e1-8c39a32c821a
+for temperature = ZebrafishHMM2023.artr_wolf_2023_temperatures(), fish = ZebrafishHMM2023.artr_wolf_2023_fishes(temperature)
+	@assert all(diff(artr_data[(; temperature, fish)].time) .≈ mean(diff(artr_data[(; temperature, fish)].time)))
+end
 
-# ╔═╡ 60937be5-8526-4e3f-a17d-d4bb2f6e06a7
-ZebrafishHMM2023.load_full_obs(26)[1][1]
+# ╔═╡ 3b6b3cc3-d29f-469b-ad3a-e0cc50017b02
+artr_time_unit = Dict((; temperature, fish) =>
+	mean(diff(artr_data[(; temperature=26, fish=6)].time))
+	for temperature = ZebrafishHMM2023.artr_wolf_2023_temperatures()
+	for fish = ZebrafishHMM2023.artr_wolf_2023_fishes(temperature)
+)
+
+# ╔═╡ 37ab290b-e7b6-42b2-bccf-7d73c1916557
+md"# Train behavior models"
+
+# ╔═╡ 88d1dfc7-e0c6-47c0-8170-787e290fc2c8
+function train_bouts_hmm(T::Int)
+	@info "Training models for T = $T"
+
+    trajs = ZebrafishHMM2023.load_behaviour_free_swimming_trajs(T)
+
+    trajs = filter(traj -> all(!iszero, traj), trajs) # zeros give trouble sometimes
+    hmm = ZebrafishHMM2023.normalize_all!(ZebrafishHMM2023.ZebrafishHMM_G3_Sym(rand(), rand(3,3), 1.0, Gamma(1.5, 20.0), 1.0))
+    (hmm, lL) = HiddenMarkovModels.baum_welch(hmm, trajs, length(trajs); max_iterations = 500, check_loglikelihood_increasing = false, atol = ZebrafishHMM2023.ATol(1e-6))
+
+    return hmm
+end
+
+# ╔═╡ 0601dac5-cef9-4b2b-83ce-d37064e4d9e1
+bouts_hmms = Dict(temperature => train_bouts_hmm(temperature) for temperature = ZebrafishHMM2023.behaviour_free_swimming_temperatures())
+
+# ╔═╡ 369d6c55-4239-4eeb-b23b-4c3910d791fd
+md"# Sample behavior"
+
+# ╔═╡ edd47451-04d6-4fe1-ab6c-61ca175e9e4f
+function sample_behavior_states_from_artr(; temperature::Int, fish::Int, N::Int, λ::Real)
+	all_bout_times = [obs.t for traj = ZebrafishHMM2023.load_full_obs(temperature) for obs = traj]
+	rescaled_bout_times = all_bout_times * λ / artr_time_unit[(; temperature, fish)]
+	
+	all_states = artr_viterbi_states[(; temperature, fish)]
+
+	selected_indices = filter(≤(length(all_states)), round.(Int, cumsum(rand(rescaled_bout_times, N))))
+
+	return all_states[selected_indices]
+end
+
+# ╔═╡ a690d7e4-106c-40f3-8442-5545b8c50c94
+sample_behavior_states_from_artr(; temperature=26, fish=6, N=100, λ=2.775)
 
 # ╔═╡ Cell order:
 # ╠═5b3604a6-3dcc-11ef-1ff1-a71b51c7d8e9
@@ -68,6 +117,8 @@ ZebrafishHMM2023.load_full_obs(26)[1][1]
 # ╠═f9ed9420-cf6a-4c65-9e46-6e3e8fc54476
 # ╠═7571c197-a647-4c2e-8510-a05b7cbc447f
 # ╠═44b6d472-e1e2-4e6b-970a-182bbe07f72f
+# ╠═7d355591-dddd-4463-97b1-d53e223bd6b3
+# ╠═8398c332-235b-4f6c-ab1e-8b5fd62d2d5a
 # ╠═4f8b021f-7917-4689-a95e-02e20a766b3a
 # ╠═a67c5bfa-02b5-41af-8bbe-5ca350bfb58f
 # ╠═ee9ae8b1-e589-4c71-8450-0b9ea915df21
@@ -75,5 +126,11 @@ ZebrafishHMM2023.load_full_obs(26)[1][1]
 # ╠═a6c708f3-7f96-416e-8aca-1a7c02a21e98
 # ╠═ac410781-0712-45dd-b3eb-713b5629c329
 # ╠═fd8ef759-e4e2-4a8b-8fa7-bafad992ef7a
+# ╠═7ae067c1-8c0c-4f91-b8e1-8c39a32c821a
+# ╠═3b6b3cc3-d29f-469b-ad3a-e0cc50017b02
+# ╠═37ab290b-e7b6-42b2-bccf-7d73c1916557
+# ╠═88d1dfc7-e0c6-47c0-8170-787e290fc2c8
+# ╠═0601dac5-cef9-4b2b-83ce-d37064e4d9e1
 # ╠═369d6c55-4239-4eeb-b23b-4c3910d791fd
-# ╠═60937be5-8526-4e3f-a17d-d4bb2f6e06a7
+# ╠═edd47451-04d6-4fe1-ab6c-61ca175e9e4f
+# ╠═a690d7e4-106c-40f3-8442-5545b8c50c94
